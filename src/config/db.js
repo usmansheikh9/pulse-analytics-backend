2c26034ac4821e1b58df9a323ee5ead75cb4888c
Usman Sheikh <usmansheikh0950@gmail.com>
2026-03-15 17:00:00 +0000

    Dockerfile and production config

--- src/config/db.js
+++ src/config/db.js
@@ -6,7 +6,7 @@ const pool = new Pool({
   ssl: { rejectUnauthorized: false },
   max: 10,
   idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
+  connectionTimeoutMillis: 10000,
 })
 
 pool.on('error', err => {
